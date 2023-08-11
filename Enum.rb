ade67455a9c3cdb5b4db1fc6ab6bfd263c0adc13
Valentine <marlosgama@outlook.com>
2023-08-10 21:14:29 -0300

    Update Enum.rb

--- Enum.rb
+++ Enum.rb
@@ -14,7 +14,9 @@ def enum(constant_names)
         n = constant_names[i + 1].to_i
         constant_names.delete_at(i + 1)
       end
+      
       mod.const_set(const.to_s, n)
+      
       n += 1
     end
   end
