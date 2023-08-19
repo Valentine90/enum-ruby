a195ea13d644015f14dbfdfe62776019112d476e
Valentine <marlosgama@outlook.com>
2023-08-19 12:57:47 -0300

    Update enum.rb

--- enum.rb
+++ enum.rb
@@ -1,16 +1,14 @@
-#==============================================================================
-# ** Enum
-#------------------------------------------------------------------------------
-#  Este script lida com tipos de enumeração.
-#------------------------------------------------------------------------------
-#  Autor: Valentine
-#==============================================================================
+# frozen_string_literal: true
+
+# Lida com tipos de enumeração.
+#
+# Copyright (c) 2020 Valentine.
 
 def enum(constant_names)
   Module.new do |mod|
     n = 0
     constant_names.each_with_index do |const, i|
-      if constant_names[i + 1].to_i > 0
+      if constant_names[i + 1].to_i.positive?
         n = constant_names[i + 1].to_i
         constant_names.delete_at(i + 1)
       end
